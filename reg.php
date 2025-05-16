f273ff41289fc35c8ea81d39c567a6109001884a
vanmik <vanmikyt@mail.ru>
2025-05-16 12:52:47 +0500

    страница регистрации

---
+++ reg.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reg.css">
+    <title>Регистрация</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header_inner">
+                <div class="logo"><img src="./img/logo.png" alt=""></div>
+                <div class="menu">
+                    <div class="palka"></div>
+                    <div class="palka"></div>
+                    <div class="palka"></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <h1>Регистрация</h1>
+        <div class="block_reg">
+            <p>Процесс только для сотрудников техникума</p>
+            <div class="line"></div>
+            <input type="text" placeholder="ФИО">
+            <input type="text" placeholder="Логин">
+            <input type="text" placeholder="Пароль">
+            <div class="checkbox">
+                <input type="checkbox">
+                <p>Вы соглашаетесь на ожидание подтверждения вашей<br> учётной записи администратором</p>      
+            </div>
+            <input type="submit" value="Зарегистрироваться">
+        </div>
+        <p>Уже есть учетная запись? <a href="#">Войти</a></p>
+    </main>
+</body>
+</html>
