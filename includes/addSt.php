1dc7b73b811c65d8ad531a964931dd2d269609ca
shereposhek <childdxxxmn@gmail.com>
2025-05-18 22:15:44 +0500

    добавление нового студента

---
+++ includes/addSt.php
@@ -0,0 +1,39 @@
+<div class="table">
+    <form action="" method="post">
+        <input type="text" name="surName" placeholder="Фамилия" >
+        <input type="text" name="Name" placeholder="Имя" >
+        <input type="text" name="FathName" placeholder="Отчество" >
+        <input type="date" name="DOB" placeholder="Дата рождения в формате ДД.ММ.ГГ" >
+        <select name="Gender">
+            <option value="">---Выберите пол---</option>
+            <option value="Male">М</option>
+            <option value="Female">Ж</option>
+        </select>
+        <input type="text" name="Phone" placeholder="Номер телефона" >
+        <select name="Education">
+            <option value="">---Выберите законченые классы---</option>
+            <option value="Nine">9 классов</option>
+            <option value="Eleven">11 классов</option>
+        </select>
+        <input type="number" name="Code" placeholder="Код отделения" >
+        <input type="text" name="Group" placeholder="Группа" >
+        <input type="" name="Funding" placeholder="Тип обучения" >
+        <select name="Funding">
+            <option value="">---Выберите тип обучения---</option>
+            <option value="Free">бюджет</option>
+            <option value="Paid">внебюджет</option>
+        </select>
+        <input type="date" name="dateAdmin" placeholder="Дата поступления" >
+        <input type="date" name="dateEnd" placeholder="Дата выпуска" >
+        <select name="Expelled">
+            <option value="">---Выберите статус отчисления---</option>
+            <option value="Y">Да</option>
+            <option value="N">Нет</option>
+        </select>
+        <input type="date" name="DateExp" placeholder="Дата отчисления" >
+        <input type="text" name="ParentInfo" placeholder="Информация о родителях" >
+        <input type="number" name="Penalties" placeholder="Взыскания" >
+        <input type="text" name="Notes" placeholder="Дополнительно" >
+        <input type="submit" value="Добавить">
+    </form>
+</div>
