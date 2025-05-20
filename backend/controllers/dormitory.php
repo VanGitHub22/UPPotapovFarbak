a05f804d5c4d56e66ec8134ceb0dadc010b34e89
farback <000shereposhek000@gmail.com>
2025-05-20 12:47:52 +0500

    контроллер общежития

---
+++ backend/controllers/dormitory.php
@@ -0,0 +1,34 @@
+<?php
+    include '../../connection/connection.php';
+    include('../classes/dormitory.php');
+
+    $action = $_POST["action"];
+
+    if($action == "get"){
+        echo json_encode(Dormitory::Get());
+    } else if ($action == "add") {
+        $id = $_POST['id'];
+        $student_id = $_POST['student_id'];
+        $room_id = $_POST['room_id'];
+        $orderNum = $_POST['orderNum'];
+        $checkInDate = $_POST['checkInDate'];
+        $checkOutDate = $_POST['checkOutDate'];
+        $notes = $_POST['notes'];
+
+        $dorm = new Dormitory($id, $student_id, $room_id, $orderNum, $checkInDate, $checkOutDate, $notes);
+
+        $result = $dorm->Insert();
+    } else if ($action == "edit"){
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+
+        $dorm = new Dormitory($id, $student_id, $room_id, $orderNum, $checkInDate, $checkOutDate, $notes);
+
+        $result = $dorm->Update();
+        
+    } else if ($action == "delete"){
+        $id = $_POST['id'];
+        $dorm = new Dormitory($id);
+        $result = $dorm->Delete();
+    }
+?>
